20ab740995e729a0215044b79ccd67e0afa56433
yogesshraj <54570181+yogesshraj@users.noreply.github.com>
2020-06-25 17:22:59 +0530

    Add files via upload

--- model/knex.js
+++ model/knex.js
@@ -1,1 +1,11 @@
+const knex = require("knex");
 
+module.exports = require("knex")({
+    client: "mysql",
+    connection:{
+        host:"localhost",
+        user:"root",
+        password:"navgurukul",
+        database:"student_inventory"
+    }
+});
