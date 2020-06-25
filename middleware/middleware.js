43eda25bc7ec2354e87376fc423cf857a0f14dd6
yogesshraj <54570181+yogesshraj@users.noreply.github.com>
2020-06-25 17:24:39 +0530

    Add files via upload

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,1 +1,10 @@
+const jwt = require("jsonwebtoken");
 
+module.exports=function middleware(req,res,next) {
+    var authentication = jwt.verify(req.headers.authorization,"secretkey");
+    if (authentication==="yogesshrajlakshmi"){
+        next();
+    }else{
+        res.status(404).send("Access Denied");
+    }
+}
