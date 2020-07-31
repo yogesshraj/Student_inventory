89880b1f63d297fc91ae1b66216a8fc88d1eebf2
yogesshraj <54570181+yogesshraj@users.noreply.github.com>
2020-07-31 13:05:59 +0530

    Update get.js

--- controller/get.js
+++ controller/get.js
@@ -5,17 +5,11 @@ exports.getall = (req,res)=>{
         .then(result=>{
             res.status(202).send(result)
         })
-        .catch(err=>{
-            res.status(404).send(err);
-        });
 };
 
 exports.getId = (req,res)=>{
     knex.select("*").from("data_table").where("username",req.params.username)
         .then(result=>{
             res.status(202).send(result)
-        })
-        .catch(err=>{
-            res.status(404).send(err);
         })
 }
